5c15afaaf76fe1f533cc44ecee8603b6eda58c8b
emmanuellekamwa <emmanuellekamwa@gmail.com>
2021-01-29 08:34:05 +0100

    Add comments

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :comments
   devise_for :users
   resources :links do
    member do
@@ -6,5 +7,6 @@ Rails.application.routes.draw do
     put "dislike", to: "links#downvote"
    end
   end
+   resources :comments
   root to: 'links#index'
 end
