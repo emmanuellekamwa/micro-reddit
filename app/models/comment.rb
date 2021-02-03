5c7bd087ad98d928d3d8535eb4a3bf610c1b98a8
emmanuellekamwa <emmanuellekamwa@gmail.com>
2021-02-03 12:28:46 +0100

    Add post model

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,9 @@
 class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :link
+
+  validates :title, presence: true, length: { in: 3..40 }
+  validates :body, presence: true
+  validates :user_id, presence: true
+  validates :post_id, presence: true
 end
